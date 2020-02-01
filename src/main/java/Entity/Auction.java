14fb10af260664e6a1d628e0aa131e351d5e8b43
AvivMoyal <Aviv.Moyak@gmail.com>
2020-02-01 02:49:02 +0200

    added service methods

--- src/main/java/Entity/Auction.java
+++ src/main/java/Entity/Auction.java
@@ -1,9 +1,15 @@
 package Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash("Auction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Auction {
     @Id
     int id;
--- src/main/java/service/BankerService.java
+++ src/main/java/service/BankerService.java
@@ -1,8 +1,10 @@
 package service;
 
+import Entity.Auction;
 import Entity.Campaign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.AuctionRepository;
 import repository.CampaignRepository;
 
 import java.util.HashMap;
@@ -11,11 +13,13 @@ import java.util.Map;
 @Service
 public class BankerService {
     private final CampaignRepository campaignRepository;
+    private final AuctionRepository auctionRepository;
     private Map<Integer, Integer> campaignsBudgets;
 
     @Autowired
-    public BankerService(CampaignRepository campaignRepository) {
+    public BankerService(CampaignRepository campaignRepository, AuctionRepository auctionRepository) {
         this.campaignRepository = campaignRepository;
+        this.auctionRepository = auctionRepository;
         initBudgets();
     }
 
@@ -23,5 +27,25 @@ public class BankerService {
         this.campaignsBudgets = new HashMap<>();
         this.campaignRepository.findAll().forEach(c -> this.campaignsBudgets.put(c.getId(), c.getBudget()));
 
+    }
+
+    public boolean check(int campaignId, int auctionId, int price) {
+        int budget = this.campaignsBudgets.get(campaignId);
+        if (price < budget) {
+            this.campaignsBudgets.replace(campaignId, budget - price);
+            this.auctionRepository.save(new Auction(auctionId, campaignId, price));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void updateAuctionLost(int auctionId) {
+        // TODO: check auction exist
+        int campaignIid = this.auctionRepository.findById(auctionId).get().getCampaignId();
+        int price = this.auctionRepository.findById(auctionId).get().getPrice();
+
+        int budget = this.campaignsBudgets.get(campaignIid);
+        this.campaignsBudgets.replace(campaignIid, budget + price);
     }
 }
